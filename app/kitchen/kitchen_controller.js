290ca39ef2847097df319d9b2640eafc0633908f
zhiyan.wang <zhiyan1987@gmail.com>
2015-09-06 17:01:49 +0800

    update

--- app/kitchen/kitchen_controller.js
+++ app/kitchen/kitchen_controller.js
@@ -21,27 +21,42 @@
       $scope.productPage = 1;
       $scope.newsPage = 1;
       $scope.dotlistStyle = {};
+      $scope.currentPage = 1;
+      $scope.pages = [];
+
+      var originProductList = [],
+          originNewsList = []
 
       $scope.pageTo = function( page ){
+        var start,
+            end;
+        if( page <= $scope.productPage ){
+          start = (page-1)*6;
+          end = start+6;
+          $scope.productList = originProductList.slice(start,end)
+        }else{
+          start = (page-$scope.productPage-1)*3;
+          end = start+3;
+          $scope.newsList = originNewsList.slice(start,end)
+        }
+        $scope.currentPage = page;
+      }
 
-        $q.all([
-            $http.get("/api/bodhi/query/product.htm",{params: {"page":Math.min(page,$scope.productPage),"type":1}}), 
-            $http.get("/api/bodhi/query/news.htm",{params: {"page":Math.max(page-$scope.productPage,1),"type":1}})])
+      $q.all([
+            $http.get("/api/bodhi/query/product.htm",{params: {"type":1}}), 
+            $http.get("/api/bodhi/query/news.htm",{params: {"type":1}})])
           .then(function(res){
             var productData = res[0].data,
                 newsData = res[1].data;
 
-            $scope.currentPage = page;
-            $scope.pages = [];
-
             if( productData.ret ){
-                $scope.productList = productData.data.list;
-                $scope.productPage = productData.data.pageCount;
+                originProductList = productData.data.list;
+                $scope.productPage = Math.ceil(productData.data.list.length/6);
             }
 
             if( newsData.ret ){
-                $scope.newsList = newsData.data.list;
-                $scope.newsPage = newsData.data.pageCount;
+                originNewsList = newsData.data.list;
+                $scope.newsPage = Math.ceil(newsData.data.list.length/3)
             }
 
             for(var i = 1;i<=$scope.newsPage+$scope.productPage;i++){
@@ -49,11 +64,9 @@
             }
             $scope.dotlistStyle = {"margin-top":-31*$scope.pages.length/2-20+"px"}
 
+            $scope.pageTo(1)
+
           })
-
-      }
-
-      $scope.pageTo(1);
     });
 
 })();
