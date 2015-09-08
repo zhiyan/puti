a15f5d7cf07b2862ab768fef3040d574cdc7da8b
zhiyan.wang <zhiyan1987@gmail.com>
2015-09-08 22:07:31 +0800

    update

--- build/app.js
+++ build/app.js
@@ -375,57 +375,6 @@
   'use strict';
 
 
-  angular.module('view-main',['ngRoute'])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'main/main.html',
-          controller: 'MainCtrl'
-        });
-    })
-    .controller('MainCtrl', function ($scope,$http) {
-
-      $scope.changeNav("");
-      $scope.changeBg("");
-
-      $http.get("/api/bodhi/query/home.htm")
-          .success(function(res){
-            var list = [];
-            if(res.ret){
-              $.each(res.data,function(i,v){
-                if(!!v.imgUrl && v.imgUrl !== "#" ){
-                  list.push(v)
-                }
-              })
-              $scope.list = list;
-            }
-          })
-
-      angular.element("#js-date-from").pickadate()
-      angular.element("#js-date-to").pickadate()
-
-      // pickadate插件对angular有影响，只能用jq方式，原因待查
-      angular.element("#js-submit").on("click",function(){
-        var from = $("#js-date-from").val(),
-            to = $("#js-date-to").val();
-        if( !!from && !!to){
-          window.location.href="#/order/"+from+"/"+to
-        }
-      })
-
-
-      // $('.flexslider').flexslider({
-      //   animation: "slide",
-      //   directionNav : false
-      // });
-      
-    });
-
-})();
-(function(){
-  'use strict';
-
-
   angular.module('view-order',['ngRoute'])
     .config(function ($routeProvider) {
       $routeProvider
@@ -472,6 +421,57 @@
 
       angular.element("#js-date-from").pickadate()
       angular.element("#js-date-to").pickadate()
+    });
+
+})();
+(function(){
+  'use strict';
+
+
+  angular.module('view-main',['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'main/main.html',
+          controller: 'MainCtrl'
+        });
+    })
+    .controller('MainCtrl', function ($scope,$http) {
+
+      $scope.changeNav("");
+      $scope.changeBg("");
+
+      $http.get("/api/bodhi/query/home.htm")
+          .success(function(res){
+            var list = [];
+            if(res.ret){
+              $.each(res.data,function(i,v){
+                if(!!v.imgUrl && v.imgUrl !== "#" ){
+                  list.push(v)
+                }
+              })
+              $scope.list = list;
+            }
+          })
+
+      angular.element("#js-date-from").pickadate()
+      angular.element("#js-date-to").pickadate()
+
+      // pickadate插件对angular有影响，只能用jq方式，原因待查
+      angular.element("#js-submit").on("click",function(){
+        var from = $("#js-date-from").val(),
+            to = $("#js-date-to").val();
+        if( !!from && !!to){
+          window.location.href="#/order/"+from+"/"+to
+        }
+      })
+
+
+      // $('.flexslider').flexslider({
+      //   animation: "slide",
+      //   directionNav : false
+      // });
+      
     });
 
 })();
