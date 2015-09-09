df793c8a9bca987606794cce9d3269cc867282d4
zhiyan.wang <zhiyan1987@gmail.com>
2015-09-09 10:35:47 +0800

    update

--- app/main/main_controller.js
+++ app/main/main_controller.js
@@ -33,6 +33,8 @@
 
       // pickadate插件对angular有影响，只能用jq方式，原因待查
       angular.element("#js-submit").on("click",function(){
+        window.location.href = "http://hotels.ctrip.com/Domestic/ShowHotelInformation.aspx?hotel=1212522&relatedate=2&utm_source=baidu&utm_medium=cpc&utm_campaign=baidu72&campaign=CHNbaidu72&adid=H1212522&gclid=&isctrip=T";
+        return false;
         var from = $("#js-date-from").val(),
             to = $("#js-date-to").val();
         if( !!from && !!to){
